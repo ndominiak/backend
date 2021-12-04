04e5bc8cb099e89135fac8023e3d7e5d9becdb14
Elisandro Moreira <elisandro.moreira@univille.br>
2021-12-04 13:55:03 -0300

    feat: register samples repository in container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
 import { PatientsRepository } from "@modules/patients/infra/typeorm/repositories/PatientsRepository";
+import { SamplesRepository } from "@modules/patients/infra/typeorm/repositories/SamplesRepository";
 import { IPatientsRepository } from "@modules/patients/repositories/IPatientsRepository";
+import { ISamplesRepository } from "@modules/patients/repositories/ISamplesRepository";
 
 container.registerSingleton<IPatientsRepository>(
   "PatientsRepository",
   PatientsRepository
+);
+
+container.registerSingleton<ISamplesRepository>(
+  "SamplesRepository",
+  SamplesRepository
 );
