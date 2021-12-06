482772ea0a846c31965309793acc6188b40adc4a
Elisandro Moreira <elisandro.moreira@univille.br>
2021-12-05 23:21:51 -0300

    feat: register control samples repository in container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { container } from "tsyringe";
 
+import { ControlSamplesRepository } from "@modules/patients/infra/typeorm/repositories/ControlSamplesRepository";
 import { PatientsRepository } from "@modules/patients/infra/typeorm/repositories/PatientsRepository";
 import { SamplesRepository } from "@modules/patients/infra/typeorm/repositories/SamplesRepository";
+import { IControlSamplesRepository } from "@modules/patients/repositories/IControlSamplesRepository";
 import { IPatientsRepository } from "@modules/patients/repositories/IPatientsRepository";
 import { ISamplesRepository } from "@modules/patients/repositories/ISamplesRepository";
 
@@ -13,4 +15,9 @@ container.registerSingleton<IPatientsRepository>(
 container.registerSingleton<ISamplesRepository>(
   "SamplesRepository",
   SamplesRepository
+);
+
+container.registerSingleton<IControlSamplesRepository>(
+  "ControlSamplesRepository",
+  ControlSamplesRepository
 );
