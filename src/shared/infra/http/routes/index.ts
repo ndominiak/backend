052f8d7cf9036e402cdd1eada24eb0db02674984
Elisandro Moreira <elisandro.moreira@univille.br>
2021-12-05 23:41:10 -0300

    feat: add control sample route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { controlsRoutes } from "./controls.routes";
 import { patientsRoutes } from "./patients.routes";
 import { samplesRoutes } from "./samples.routes";
 
@@ -7,5 +8,6 @@ const router = Router();
 
 router.use("/patients", patientsRoutes);
 router.use("/samples", samplesRoutes);
+router.use("/controls", controlsRoutes);
 
 export { router };
