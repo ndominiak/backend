66b75be0e119791a279c057d701361f1adc02451
Elisandro Moreira <elisandro.moreira@univille.br>
2022-01-22 16:53:07 -0300

    feat: add user routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { controlsRoutes } from "./controls.routes";
 import { patientsRoutes } from "./patients.routes";
 import { samplesRoutes } from "./samples.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/patients", patientsRoutes);
 router.use("/samples", samplesRoutes);
 router.use("/controls", controlsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
