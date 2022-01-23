e5a080526f3eefc65a3f8e39eace09969873d735
Elisandro Moreira <elisandro.moreira@univille.br>
2022-01-23 16:08:19 -0300

    feat: add authenticate user controller

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { registration, password } = request.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const token = await authenticateUserUseCase.execute({
+      registration,
+      password,
+    });
+
+    return response.json(token);
+  }
+}
+
+export { AuthenticateUserController };
