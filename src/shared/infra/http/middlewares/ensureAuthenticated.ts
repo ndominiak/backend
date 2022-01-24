eb6eaaa3e890c4467bc96dfd2686c7d76838458d
Elisandro Moreira <elisandro.moreira@univille.br>
2022-01-23 23:08:04 -0300

    feat: add ensure authenticated middleware

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository.";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "0828e69357b402dce5fc11a5299361e7"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User not found");
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token");
+  }
+}
