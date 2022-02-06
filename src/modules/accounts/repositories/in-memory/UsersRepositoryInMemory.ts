e7b8a4c7506ac553199b16716c067fd6ec30c1d3
Elisandro Moreira <elisandro.moreira@univille.br>
2022-02-06 14:02:57 -0300

    feat: create UsersRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,40 @@
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    name,
+    registration,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      registration,
+      email,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findByRegistration(registration: string): Promise<User> {
+    return this.users.find((user) => user.registration === registration);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
